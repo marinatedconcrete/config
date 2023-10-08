b3ed5ea9bc5cac9c75f6465bacf0f818d5765fc3
Rob Arnold <326113+robarnold@users.noreply.github.com>
2023-10-08 10:46:15 -0700

    Add tons of comments to help explain all the infra

--- images/kairos-zsh-bundle/run.sh
+++ images/kairos-zsh-bundle/run.sh
@@ -4,4 +4,6 @@ set -ex
 
 DEST="/home/kairos"
 
+# Copy the zsh configs to the kairos user homedir with the appropriate permissions.
+# User writable is helpful for local experimentation/debugging
 install -m u+rw-x,o-rwx,g-rwx -o kairos -g kairos -t "${DEST}" assets/
