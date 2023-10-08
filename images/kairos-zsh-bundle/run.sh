eb7fde84d140dfa6f12a41fd51c9f7e5d3419135
Rob Arnold <326113+robarnold@users.noreply.github.com>
2023-10-08 11:16:29 -0700

    Try to build a zsh bundle (#34)
    This is supposed to install the zsh configs defined by our Ansible role
    into /home/kairos

---
+++ images/kairos-zsh-bundle/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+DEST="/home/kairos"
+
+# Copy the zsh configs to the kairos user homedir with the appropriate permissions.
+# User writable is helpful for local experimentation/debugging
+install -m u+rw-x,o-rwx,g-rwx -o kairos -g kairos -t "${DEST}" assets/
