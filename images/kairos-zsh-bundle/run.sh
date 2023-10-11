67628a83e07808eb853e3c7ab8bebb277a02d647
Rob Arnold <326113+robarnold@users.noreply.github.com>
2023-10-11 13:33:20 -0700

    Explicit loop

--- images/kairos-zsh-bundle/run.sh
+++ images/kairos-zsh-bundle/run.sh
@@ -6,4 +6,6 @@ DEST="/home/kairos"
 
 # Copy the zsh configs to the kairos user homedir with the appropriate permissions.
 # User writable is helpful for local experimentation/debugging
-install -m u+rw-x,o-rwx,g-rwx -o kairos -g kairos -t "${DEST}" assets/*
+for config in assets/*; do
+  install -m u+rw-x,o-rwx,g-rwx -o kairos -g kairos -t "${DEST}" "${config}"
+done
