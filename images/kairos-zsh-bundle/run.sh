5106c23746382bd9bca10379ab3c3cbd7220a8a0
Rob Arnold <326113+robarnold@users.noreply.github.com>
2023-10-22 19:49:54 -0700

    restore asset path

--- images/kairos-zsh-bundle/run.sh
+++ images/kairos-zsh-bundle/run.sh
@@ -12,5 +12,5 @@ find .
 # Copy the zsh configs to the kairos user homedir with the appropriate permissions.
 # User writable is helpful for local experimentation/debugging
 for config in `ls -A ./assets`; do
-  install -m u+rw-x,o-rwx,g-rwx -o kairos -g kairos -t "${DEST}" "${config}"
+  install -m u+rw-x,o-rwx,g-rwx -o kairos -g kairos -t "${DEST}" "assets/${config}"
 done
