8235b04705e7e11f6d3a8a50bbc5100a523f11c3
Rob Arnold <326113+robarnold@users.noreply.github.com>
2023-10-22 19:33:15 -0700

    log and relative

--- images/kairos-zsh-bundle/run.sh
+++ images/kairos-zsh-bundle/run.sh
@@ -4,8 +4,12 @@ set -ex
 
 DEST="/home/kairos"
 
+echo $PATH
+pwd
+whoami
+
 # Copy the zsh configs to the kairos user homedir with the appropriate permissions.
 # User writable is helpful for local experimentation/debugging
-for config in /assets/*; do
+for config in assets/*; do
   install -m u+rw-x,o-rwx,g-rwx -o kairos -g kairos -t "${DEST}" "${config}"
 done
