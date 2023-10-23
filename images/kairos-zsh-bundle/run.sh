55c96e488abe1b18fb0dc4f785f79e1ceba58d78
Rob Arnold <326113+robarnold@users.noreply.github.com>
2023-10-22 20:20:22 -0700

    Use proper glob for installation script (#45)
    This gets the files installed right when manually installed

--- images/kairos-zsh-bundle/run.sh
+++ images/kairos-zsh-bundle/run.sh
@@ -4,6 +4,11 @@ set -ex
 
 DEST="/home/kairos"
 
+# Make sure we can find the dot files when we glob
+shopt -s dotglob
+
 # Copy the zsh configs to the kairos user homedir with the appropriate permissions.
 # User writable is helpful for local experimentation/debugging
-install -m u+rw-x,o-rwx,g-rwx -o kairos -g kairos -t "${DEST}" assets/
+for config in ./assets/*; do
+  install -m u+rw-x,o-rwx,g-rwx -o kairos -g kairos -t "${DEST}" "${config}"
+done
