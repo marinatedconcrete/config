5bb8e3c0de74acf7e82a82ad3e8626fb6ecca3e4
Rob Arnold <326113+robarnold@users.noreply.github.com>
2023-10-22 19:44:22 -0700

    glob hidden

--- images/kairos-zsh-bundle/run.sh
+++ images/kairos-zsh-bundle/run.sh
@@ -11,6 +11,6 @@ find .
 
 # Copy the zsh configs to the kairos user homedir with the appropriate permissions.
 # User writable is helpful for local experimentation/debugging
-for config in assets/*; do
+for config in `ls -A ./assets`; do
   install -m u+rw-x,o-rwx,g-rwx -o kairos -g kairos -t "${DEST}" "${config}"
 done
