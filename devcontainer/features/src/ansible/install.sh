726ef2129619f0d83805f761b78a4cb5081df3a4
Rob Arnold <326113+robarnold@users.noreply.github.com>
2023-10-28 21:39:48 -0700

    Try adding a test and include the pip requirements (#69)

--- devcontainer/features/src/ansible/install.sh
+++ devcontainer/features/src/ansible/install.sh
@@ -6,13 +6,19 @@ if [ -f requirements.txt ]; then
     if ! command -v pip; then
         sudo apt update
         sudo apt install -y --no-install-recommends python3-pip
+    else
+        echo "Pip already installed"
     fi
     pip install -r requirements.txt
+else
+    echo "No requirements.txt found"
 fi
 
 # Now that pip is setup, install Ansible Galaxy modules
 if [ -f requirements.yml ]; then
     ansible-galaxy install -r requirements.yml
+else
+    echo "No requirements.yml found"
 fi
 
 # sshpass for initial node provisioning
@@ -21,5 +27,10 @@ if ! command -v sshpass; then
     sudo apt install -y --no-install-recommends sshpass
 fi
 
+# Actually run the playbook
 PLAYBOOK=${PLAYBOOK:-undefined}
-ansible-playbook "$PLAYBOOK"
+if [ -f "$PLAYBOOK" ]; then
+    ansible-playbook "$PLAYBOOK"
+else
+    echo "Could not find playbook - cowardly doing nothing"
+fi
---
+++ devcontainer/features/test/ansible/basic.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
+# Provides the 'check' and 'reportResults' commands.
+ # shellcheck disable=SC1091
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib. Syntax is...
+# check <LABEL> <cmd> [args...]
+check "installed pip" bash -c "command -v pip"
+check "installed ansible-playbook" bash -c "command -v ansible-playbook"
+
+# Report results
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
