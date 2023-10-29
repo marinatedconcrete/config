aebbfdd272604fe26406b9ecb5d6ffdf5bec6781
Rob Arnold <326113+robarnold@users.noreply.github.com>
2023-10-28 21:13:03 -0700

    Move to more traditional src/test setup for tests, fix lint

--- devcontainer/features/test/ansible/test.sh
+++ devcontainer/features/test/ansible/test.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
 set -e
 
 # Optional: Import test library bundled with the devcontainer CLI
 # See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
 # Provides the 'check' and 'reportResults' commands.
+ # shellcheck disable=SC1091
 source dev-container-features-test-lib
 
 # Feature-specific tests
