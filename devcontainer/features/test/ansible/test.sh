71a6ac07b2aae4dc7b72a898b38ba2f3b39cb502
Rob Arnold <326113+robarnold@users.noreply.github.com>
2023-10-28 21:14:54 -0700

    Fix lint

--- devcontainer/features/test/ansible/test.sh
+++ devcontainer/features/test/ansible/test.sh
@@ -5,7 +5,7 @@ set -e
 # See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
 # Provides the 'check' and 'reportResults' commands.
  # shellcheck disable=SC1091
-source dev-container-features-test-lib
+. dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
