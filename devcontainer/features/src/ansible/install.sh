c13c8e913d646698c40c6a0d09c407883279e5dc
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2023-10-30 17:20:15 -0700

    restructure into two phases

--- devcontainer/features/src/ansible/install.sh
+++ devcontainer/features/src/ansible/install.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 set -e
 
+#
+# Gather & Install Required Packages
+#
+
+# This will contain all the packages we need to install; space seperated.
 packages="gnupg2"
+
 if [ -f requirements.txt ]; then
     # Install pip to install ansible & kubernetes modules
     if ! command -v pip; then
         packages="$packages python3-pip"
-    else
-        echo "Pip already installed"
     fi
+fi
+
+# sshpass for initial node provisioning
+if ! command -v sshpass; then
+    packages="$packages sshpass"
+fi
+
+sudo apt update
+sudo apt install -y --no-install-recommends "$packages"
+
+#
+# Non-Package Requirements
+#
+
+if [ -f requirements.txt ]; then
     pip install -r requirements.txt
 else
     echo "No requirements.txt found"
@@ -20,11 +39,3 @@ if [ -f requirements.yml ]; then
 else
     echo "No requirements.yml found"
 fi
-
-# sshpass for initial node provisioning
-if ! command -v sshpass; then
-    packages="$packages sshpass"
-fi
-
-sudo apt update
-sudo apt install -y --no-install-recommends "$packages"
