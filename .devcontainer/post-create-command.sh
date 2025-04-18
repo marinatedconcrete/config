04048b89b8bdb8151e01564c32b1d93dc0f13fcf
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2025-04-18 14:59:12 -0700

    fix: workaround docker-in-docker issue on newer fedora kernel
    As noted in https://github.com/devcontainers/features/issues/1235, the
    docker-in-docker feature simply always forces the use of
    `iptables-legacy`.  This doesn't work in newer fedora kernels, and so
    this just does a quick `uname` check to verify that we have it, and
    swaps to `iptables-nftables` if it matches.

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -6,3 +6,8 @@ set -e
 yarn --immutable
 
 ansible-galaxy collection install --no-cache ansible
+
+# https://github.com/devcontainers/features/issues/1235
+if uname -r | grep -q '\.fc'; then
+  sudo update-alternatives --set iptables /usr/sbin/iptables-nft
+fi
