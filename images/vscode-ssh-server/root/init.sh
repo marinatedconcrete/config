7b2bcdfe25137be4872a2a460b08612fb6d284f1
Rob Arnold <326113+robarnold@users.noreply.github.com>
2025-04-26 16:33:30 -0700

    Initial version of the vscode ssh server
    Like the readme says, we can't use the linuxserver one. This is a pretty minimal one that I think will suffice.

---
+++ images/vscode-ssh-server/root/init.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+set -ex
+
+# Ensure per-machine keys
+CONFIG_HOST_KEY_PATH=/config/ssh_host_ed25519_key
+# If the config doesn't have keys, generate them
+if [ ! -f $CONFIG_HOST_KEY_PATH ]; then
+    ssh-keygen -t ed25519 -N '' -q -f $CONFIG_HOST_KEY_PATH
+fi
+
+# Grab authorized keys
+AUTHORIZED_KEYS_PATH=/home/dev/.ssh/authorized_keys
+if [ ! -f $AUTHORIZED_KEYS_PATH ]; then
+    # Ex: https://github.com/robarnold.keys
+    curl -L $AUTHORIZED_KEYS_URL -o $AUTHORIZED_KEYS_PATH
+    chmod 600 $AUTHORIZED_KEYS_PATH
+else
+    echo "Authorized keys already present. Skipping download."
+fi
+
+echo "Authorized keys:"
+cat $AUTHORIZED_KEYS_PATH
+
+# Run sshd using the host key in the config
+/usr/sbin/sshd -D -h $CONFIG_HOST_KEY_PATH
