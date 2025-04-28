159f80f2ffea0f23a03a0709d55f0f94fec32882
Rob Arnold <326113+robarnold@users.noreply.github.com>
2025-04-28 02:16:41 +0000

    init.sh comments

--- images/vscode-ssh-server/root/init.sh
+++ images/vscode-ssh-server/root/init.sh
@@ -5,12 +5,14 @@ set -ex
 CONFIG_HOST_KEY_PATH=/config/ssh_host_ed25519_key
 # If the config doesn't have keys, generate them
 if [ ! -f $CONFIG_HOST_KEY_PATH ]; then
+    echo "No ssh host key found. Generating new key into $CONFIG_HOST_KEY_PATH"
     ssh-keygen -t ed25519 -N '' -q -f $CONFIG_HOST_KEY_PATH
 fi
 
 # Grab authorized keys
 AUTHORIZED_KEYS_PATH=/home/dev/.ssh/authorized_keys
 if [ ! -f $AUTHORIZED_KEYS_PATH ]; then
+    echo "No authorized keys found. Grabbing new keys from $AUTHORIZED_KEYS_URL"
     curl --location --fail "$AUTHORIZED_KEYS_URL" -o $AUTHORIZED_KEYS_PATH
     chmod 600 $AUTHORIZED_KEYS_PATH
 else
