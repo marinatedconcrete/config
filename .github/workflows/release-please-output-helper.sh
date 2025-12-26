5ccb174a06ac3e187cea9eba48794270303fde83
Rob Arnold <326113+robarnold@users.noreply.github.com>
2025-12-26 12:33:24 -0800

    Use compact output to avoid failures for releases
    Multi-line output from JQ is likely breaking things here

--- .github/workflows/release-please-output-helper.sh
+++ .github/workflows/release-please-output-helper.sh
@@ -20,7 +20,7 @@ if [ -n "$released_paths" ]; then
         tag_name=$(jq -r --arg key "$p"--tag_name '.[$key]' /tmp/release-please-output.json)
 
         if [ "$release_created" = true ]; then
-            components=$(printf '%s' "$components" | jq --arg project "$package_name" --arg tag "$tag_name" '. + [{project: $project, tag: $tag}]')
+            components=$(printf '%s' "$components" | jq -c --arg project "$package_name" --arg tag "$tag_name" '. + [{project: $project, tag: $tag}]')
         fi
         echo "Done!"
     done <<EOF
