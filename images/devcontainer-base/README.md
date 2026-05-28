e30e017ccc2448b554a5b8f82a47be78600c3fd7
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2026-05-27 20:00:21 -0700

    feat(devcontainer-base): Create a new base image for devcontainers
    A fedora-based image that we can use in various repos, including this
    one.

---
+++ images/devcontainer-base/README.md
@@ -0,0 +1,7 @@
+# Marinated Concrete's Fedora-Based Devcontainer Image
+
+This is a Fedora-based devcontainer base image that can be used for more
+specific devcontainers.  The goal is to provide a small, minimal devcontainer
+image that works well with vscode.
+
+We automatically publish a new version weekly, or when relevant changes are made.
---
+++ images/devcontainer-base/build_files/packages.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -eoux pipefail
+
+# The set of packages that are needed for this build to work.
+CORE_PACKAGES=(
+    "curl"
+    "gzip"
+    "tar"
+)
+
+# The set of packages that we agree are too useful to not have in the base
+# image.
+COMMON_PACKAGES=(
+    # It's never DNS, but it's good to verify that...
+    "bind-utils"
+    "iproute"
+    "iputils"
+    # These are core utilities that seem silly to not include.
+    "coreutils"
+    "sudo"
+    "watch"
+    # Source Control
+    "diffutils"
+    "git"
+    # Editor of choice.
+    "vim"
+)
+
+dnf upgrade -y
+dnf install \
+    --setopt=install_weak_deps=False \
+    -y \
+    "${CORE_PACKAGES[@]}" \
+    "${COMMON_PACKAGES[@]}"
+
+# Now we can invoke some additional scripts for potentially more complicated installs.
+/ctx/packages/just.sh
